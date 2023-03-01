b55b562f3f3c92e6396dca0c6258677d9cb3e18a
Chance Hemerda <cshemerda@gmail.com>
2023-03-01 12:48:10 -0700

    add footer

--- client/components/Navbar/index.js
+++ client/components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { logout } from "../store";
+import { logout } from "../../store";
 
 const Navbar = () => {
   const [searchInput, setSearchInput] = useState("");
