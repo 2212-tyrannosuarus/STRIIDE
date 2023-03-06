7d007e77263dd783617a99077c70e89b389cd4b6
miro <miro.maleb@gmail.com>
2023-03-06 17:53:51 -0500

    cannot add to cart if inventroy is empty

--- client/reducers/singleProductPageSlice.js
+++ client/reducers/singleProductPageSlice.js
@@ -9,10 +9,25 @@ export const fetchSingleProduct = createAsyncThunk(
   }
 );
 
+export const fetchSingleProductInventory = createAsyncThunk(
+  "inventoryQuantity/get",
+  async ({ id, color, size }) => {
+    console.log(`inside thunk|${id}/${color}/${size}`);
+    const { data } = await axios.get(`/api/inventory/${id}`, {
+      headers: {
+        color: color,
+        size: size,
+      },
+    });
+    return data;
+  }
+);
+
 export const singleProductPageSlice = createSlice({
   name: "singleProductPage",
   initialState: {
     singleProduct: {},
+    singleProductInventory: 0,
     errorMsg: "",
   },
   reducers: {},
@@ -20,10 +35,16 @@ export const singleProductPageSlice = createSlice({
     build.addCase(fetchSingleProduct.fulfilled, (state, action) => {
       state.singleProduct = action.payload;
     });
+    build.addCase(fetchSingleProductInventory.fulfilled, (state, action) => {
+      state.singleProductInventory = action.payload;
+    });
   },
 });
 
 export const selectSingleProduct = (state) => {
   return state.singleProductsPage.singleProduct;
+};
+export const selectSingleProductInventory = (state) => {
+  return state.singleProductsPage.singleProductInventory;
 };
 export default singleProductPageSlice.reducer;
