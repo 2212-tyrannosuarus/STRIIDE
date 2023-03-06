09e79665d5a814989092ee78c85299d9eb2a8875
Chance Hemerda <cshemerda@gmail.com>
2023-03-06 14:07:42 -0700

    add page not found component/route

---
+++ client/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./PageNotFound.css";
+
+export const PageNotFound = () => {
+  return (
+    <div className="not-found-container">
+      <h1>404</h1>
+      <p>Oops! The page you are looking for cannot be found.</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
