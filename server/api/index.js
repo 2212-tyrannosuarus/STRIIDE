8bc8337cab9628f10c3784bb6e72a6d7cf769ae0
Tasneem Patrawala <tasneemp_sa@yahoo.com>
2023-03-07 15:03:36 -0500

    changed route for shipping

--- server/api/index.js
+++ server/api/index.js
@@ -6,7 +6,7 @@ router.use('/products', require('./products'));
 router.use('/carts', require('./carts'));
 router.use('/orders', require('./orders'));
 router.use('/inventory', require('./inventory'));
-router.use('/shippinginfo', require('./shippinginfo'));
+router.use('/shippinginfo', require('./shippingInfo'));
 router.use('/paymentmethod', require('./paymentmethod'));
 
 
