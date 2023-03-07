733bc3cf1fbe4622bb5f063aa5a41f7123c7fac3
Chance Hemerda <cshemerda@gmail.com>
2023-03-06 20:25:42 -0700

    Made seperate images folder for homepage

--- client/components/HomePage/index.js
+++ client/components/HomePage/index.js
@@ -1,14 +1,12 @@
 import React from "react";
+import {
+  FeaturedProductOne,
+  FeaturedProductTwo,
+  FeaturedProductThree,
+  FeaturedProductFour,
+} from "../Images";
 import "./HomePage.css";
 import { Link } from "react-router-dom";
-import { connect, useDispatch, useSelector } from "react-redux";
-import {
-  selectPaginatedDisplay,
-  fetchAllMenProductsPage,
-  fetchAllWomenProductsPage,
-  filters,
-  selectTotalProducts,
-} from "../../reducers/allProductsPageSlice";
 
 export const Home = () => {
   const scrollToTop = () => {
@@ -30,25 +28,25 @@ export const Home = () => {
         </div>
         <section className="featured-section">
           <div className="featured-card">
-            <img src="https://images.unsplash.com/photo-1560362614-89027598847b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
-            <Link to="">
+            <img src={FeaturedProductOne} />
+            <Link to="PATH TO WOMENS RUNNING SHOES">
               <h3 className="card-title">Womens Running Shoes</h3>
             </Link>
           </div>
           <div className="featured-card">
-            <img src="https://images.unsplash.com/photo-1500468756762-a401b6f17b46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" />
+            <img src={FeaturedProductTwo} />
             <Link to="PATH TO MENS TRAINING SHOES">
               <h3 className="card-title">Mens Training Shoes</h3>
             </Link>
           </div>
           <div className="featured-card">
-            <img src="https://images.unsplash.com/flagged/photo-1565544863956-7619785813fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+            <img src={FeaturedProductThree} />
             <Link to="PATH TO MENS LIFESTYLE SHOES">
               <h3 className="card-title">Mens Lifestyle Shoes</h3>
             </Link>
           </div>
           <div className="featured-card">
-            <img src="https://images.unsplash.com/photo-1538805060514-97d9cc17730c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+            <img src={FeaturedProductFour} />
             <Link to="/women">
               <h3 className="card-title" onClick={scrollToTop}>
                 Womens Training Shoes
--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -50,8 +50,8 @@ const LoginForm = (props) => {
         </form>
 
         <Box display="flex" alignItems="center" justifyContent="center" mt={2}>
-          <Typography>Dont have an account?</Typography>
-          <Link to="/signup">Register Here</Link>
+          <Typography>Dont have an account? </Typography>
+          <Link to="/signup"> Register Here</Link>
         </Box>
       </Box>
     </Container>
